01165106a6c98fab3beac5ff6cee132020603878
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-28 17:59:07 +0530

    added middleware and routes

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import falcon
 from falcon_cors import CORS
 
+from middleware.middleware import Middleware
 from src.extras.status import StatusResource
 from src.extras.creator import CreatorResource
 from src.extras.destroyer import DestroyerResource
+from src.user.add_superuser import AddSuperuserResource
+from src.project.add_project import AddProjectResource
+from src.project.get_my_projects import GetMyProjectsResource
 
 cors = CORS(allow_origins_list = ["http://localhost:4200"],
             allow_all_headers = True,
             allow_all_methods = True)
 
 api = falcon.API(middleware = [
-    cors.middleware
+    cors.middleware,
+    Middleware()
 ])
 
 api.add_route("/", StatusResource())
 api.add_route("/creator", CreatorResource())
 api.add_route("/destroyer", DestroyerResource())
+api.add_route("/add-superuser", AddSuperuserResource())
+api.add_route("/add-project", AddProjectResource())
+api.add_route("/get-my-projects", GetMyProjectsResource())
