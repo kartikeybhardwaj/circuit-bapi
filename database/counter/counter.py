5a0199655ea3b3adec5a669ddaa6f2e907a56868
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-28 22:40:43 +0530

    added counter methods

--- database/counter/counter.py
+++ database/counter/counter.py
@@ -24,3 +24,59 @@ class DBCounter:
                     "count": 1
                 }
             }).modified_count == 1
+
+    def getNewProjectMetaIndex(self) -> int:
+        return self.__db.counter.find_one({
+                "for": "metaProjects"
+            })["count"] + 1
+
+    def incrementProjectMetaIndex(self) -> bool:
+        return self.__db.counter.update_one({
+                "for": "metaProjects"
+            }, {
+                "$inc": {
+                    "count": 1
+                }
+            }).modified_count == 1
+
+    def getNewMilestoneMetaIndex(self) -> int:
+        return self.__db.counter.find_one({
+                "for": "metaMilestones"
+            })["count"] + 1
+
+    def incrementMilestoneMetaIndex(self) -> bool:
+        return self.__db.counter.update_one({
+                "for": "metaMilestones"
+            }, {
+                "$inc": {
+                    "count": 1
+                }
+            }).modified_count == 1
+
+    def getNewPulseMetaIndex(self) -> int:
+        return self.__db.counter.find_one({
+                "for": "metaPulses"
+            })["count"] + 1
+
+    def incrementPulseMetaIndex(self) -> bool:
+        return self.__db.counter.update_one({
+                "for": "metaPulses"
+            }, {
+                "$inc": {
+                    "count": 1
+                }
+            }).modified_count == 1
+
+    def getNewProjectIndex(self) -> int:
+        return self.__db.counter.find_one({
+                "for": "projects"
+            })["count"] + 1
+
+    def incrementProjectIndex(self) -> bool:
+        return self.__db.counter.update_one({
+                "for": "projects"
+            }, {
+                "$inc": {
+                    "count": 1
+                }
+            }).modified_count == 1
