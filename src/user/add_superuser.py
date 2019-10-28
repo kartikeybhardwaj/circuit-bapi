cae2bf2caf7db883f51e24ee37431f311ec1c8a1
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-28 18:00:08 +0530

    add superuser

---
+++ src/user/add_superuser.py
@@ -0,0 +1,57 @@
+from database.user.user import DBUser
+from database.counter.counter import DBCounter
+
+class AddSuperuserResource:
+
+    def validateUser(self, username: str) -> bool:
+        success = False
+        # TODO: check if this user really exists using LDAP
+        if (username and len(username) > 3):
+            success = True
+        return success
+
+    """
+    REQUEST:
+        "username": "<USERNAME>"
+    """
+    def on_post(self, req, resp):
+        responseObj = {
+            "responseId": 111,
+            "message": "",
+            "data": {}
+        }
+        try:
+            requestObj = req.media
+            # validate incoming username
+            if self.validateUser(requestObj.get("username", "")):
+                # TODO: get display name of this user
+                dbu = DBUser()
+                # check if user exists
+                userExists = dbu.countDocuments(requestObj["username"])
+                if userExists:
+                    # if user exists
+                    print("user exists")
+                    dbu.updateUserToSuperuser()
+                    print("user made superuser")
+                else:
+                    # if user doesn't exist
+                    print("user doesn't exist")
+                    dbc = DBCounter()
+                    index = dbc.getNewUserIndex()
+                    print("new user index fetched")
+                    dbc.incrementUserIndex()
+                    print("user index updated")
+                    dbu.insertSuperuser(
+                        index,
+                        requestObj["username"],
+                        # TODO: display name of this user will come here
+                        req.params["kartoon-fapi-incoming"]["displayname"]
+                    )
+                    print("super user inserted")
+                responseObj["responseId"] = 211
+            else:
+                responseObj["responseId"] = 110
+                responseObj["message"] = ""
+        except Exception as ex:
+            responseObj["message"] = str(ex)
+        resp.media = responseObj
