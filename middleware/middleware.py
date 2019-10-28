7fe8de50b7d8ac3bc2ae5a43bbb806b397b38fc9
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-28 22:40:55 +0530

    added fapi _id

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -17,6 +17,7 @@ class Middleware:
         """
         # TODO: username and displayname should come from fapi
         req.params["kartoon-fapi-incoming"] = {}
+        req.params["kartoon-fapi-incoming"]["_id"] = "5db6b2c04a302636a79dce4a"
         req.params["kartoon-fapi-incoming"]["username"] = "kartikey.bhardwaj"
         req.params["kartoon-fapi-incoming"]["displayname"] = "Kartikey Bhardwaj"
         req.params["kartoon-fapi-incoming"]["employeeid"] = "772780"
