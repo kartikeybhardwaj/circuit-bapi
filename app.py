48a98cc785b0339d7806c803d9005dbaca1956d5
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-29 23:47:33 +0530

    added routes

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from src.extras.status import StatusResource
 from src.extras.creator import CreatorResource
 from src.extras.destroyer import DestroyerResource
 from src.user.add_superuser import AddSuperuserResource
+from src.project.add_meta_project import AddMetaProjectResource
+from src.milestone.add_meta_milestone import AddMetaMilestoneResource
+from src.pulse.add_meta_pulse import AddMetaPulseResource
 from src.project.add_project import AddProjectResource
 from src.project.get_my_projects import GetMyProjectsResource
 
@@ -22,5 +25,8 @@ api.add_route("/", StatusResource())
 api.add_route("/creator", CreatorResource())
 api.add_route("/destroyer", DestroyerResource())
 api.add_route("/add-superuser", AddSuperuserResource())
+api.add_route("/add-meta-project", AddMetaProjectResource())
+api.add_route("/add-meta-milestone", AddMetaMilestoneResource())
+api.add_route("/add-meta-pulse", AddMetaPulseResource())
 api.add_route("/add-project", AddProjectResource())
 api.add_route("/get-my-projects", GetMyProjectsResource())
