0d38e550a92474adb3dfeb376c1663943782344d
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-30 14:21:15 +0530

    add user validation schema

---
+++ utils/req_validator/validate_add_user.py
@@ -0,0 +1,22 @@
+validate_add_user_schema = {
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string",
+            "pattern": "^[0-9a-zA-Z\.]*$",
+            "minLength": 4,
+            "maxLength": 20
+        },
+        "displayname": {
+            "type": "string",
+            "pattern": "^[0-9a-zA-Z\. ]*$",
+            "minLength": 4,
+            "maxLength": 20
+        }
+    },
+    "required": [
+        "username",
+        "displayname"
+    ],
+    "additionalProperties": False
+}
