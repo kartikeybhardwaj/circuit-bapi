19a2a6515a05d89ca62bb5b1ed9c9449c293eae1
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-02 02:52:42 +0530

    fixed get projects

--- source/project/get_projects.py
+++ source/project/get_projects.py
@@ -1,7 +1,104 @@
 from database.user.user import DBUser
 from database.project.project import DBProject
+from database.milestone.milestone import DBMilestone
+
+dbu = DBUser()
+dbpr = DBProject()
+dbm = DBMilestone()
 
 class GetProjectsResource:
+
+    def getProjectIdsForSuperuser(self) -> "list of str":
+        projectIds = [pi["_id"]["$oid"] for pi in dbpr.getAllProjectIds()]
+        return projectIds
+
+    def getProjectIdsForNonSuperuser(self, userId: str) -> "list of str":
+        # 01. fetch accessible projectIds from users collection
+        accessibleProjects = dbu.getAccessibleProjects(userId)
+        accessibleProjectIds = [api["projectId"]["$oid"] for api in accessibleProjects]
+        # 02. fetch public projectIds
+        publicProjects = dbpr.getPublicProjectIds()
+        publicProjectIds = [ppi["_id"]["$oid"] for ppi in publicProjects]
+        # 03. merge and get set of accessible and public projectIds
+        allProjectIds = list(set(accessibleProjectIds + publicProjectIds))
+        # 04. get only activeProjectIds out of allProjectIds
+        activeProjects = dbpr.getActiveProjectIdsByIds(allProjectIds)
+        projectIds = [api["_id"]["$oid"] for api in activeProjects]
+        return projectIds
+
+    def removeInactiveMilestones(self, projects: "list of dict") -> "list of dict":
+        # 01. prepare allMilestoneIds
+        allMilestoneIds = []
+        for project in projects:
+            for milestoneId in project["milestonesList"]:
+                allMilestoneIds.append(milestoneId["$oid"])
+        # 02. get only activeMilestoneIds out of allMilestoneIds
+        activeMilestones = dbm.getActiveMilesonteIdsByIds(allMilestoneIds)
+        activeMilestoneIds = [ami["_id"]["$oid"] for ami in activeMilestones]
+        # 03. remove all inactive milestoneIds
+        for project in projects:
+            for milestoneId in project["milestonesList"]:
+                if milestoneId["$oid"] not in activeMilestoneIds:
+                    project["milestonesList"].remove(milestoneId)
+        return projects
+
+    def removeInactiveMembers(self, projects: "list of dict") -> "list of dict":
+        # 01. prepare allMemberIds
+        allMemberIds = []
+        for project in projects:
+            for member in project["members"]:
+                allMemberIds.append(member["userId"]["$oid"])
+        # 02. a member can exist in different projects, get the set of allMemberIds
+        allMemberIds = list(set(allMemberIds))
+        # 03. get only activeMemberIds out of allMemberIds
+        activeMembers = dbu.getActiveUserIdsByIds(allMemberIds)
+        activeMemberIds = [ami["_id"]["$oid"] for ami in activeMembers]
+        # 04. remove all inactive memberIds
+        for project in projects:
+            for member in project["members"]:
+                if member["userId"]["$oid"] not in activeMemberIds:
+                    project["members"].remove(member)
+        # NOTE: we'll be removing members from project if they're supposed to go inactive
+        # thus, we need not to scan members individually for a project
+        return projects
+
+    def idMapForProjectIds(self, projects: "list of dict") -> dict:
+        idMap = {}
+        # 01. get title and map it to _id from projects
+        for project in projects:
+            idMap[project["_id"]["$oid"]] = project["title"]
+        return idMap
+
+    def idMapForMilestoneIds(self, projects: "list of dict") -> dict:
+        idMap = {}
+        # 01. prepare allMilestoneIds
+        allMilestoneIds = []
+        for project in projects:
+            for milestoneId in project["milestonesList"]:
+                allMilestoneIds.append(milestoneId["$oid"])
+        # 02. get [{_id, title}, ..] from getTitlesMapByIds
+        titlesMapByIds = dbm.getTitlesMapByIds(allMilestoneIds)
+        # 03. get title and map it to _id from titlesMapByIds
+        for titlesMap in titlesMapByIds:
+            idMap[titlesMap["_id"]["$oid"]] = titlesMap["title"]
+        return idMap
+    
+    def idMapForMemberIds(self, projects: "list of dict") -> dict:
+        idMap = {}
+        # 01. prepare allMemberIds
+        allMemberIds = []
+        for project in projects:
+            for member in project["members"]:
+                allMemberIds.append(member["userId"]["$oid"])
+        # 02. get [{_id, username}, ..] from getUsernamesMapByIds
+        usernamesMapByIds = dbu.getUsernamesMapByIds(allMemberIds)
+        # 03. get username and map it to _id from usernamesMapByIds
+        for usernamesMap in usernamesMapByIds:
+            idMap[usernamesMap["_id"]["$oid"]] = usernamesMap["username"]
+        return idMap
+
+    def convertMongoDBObjectsToObjects(self, projects: "list of dict") -> "list of dict":
+        pass
 
     def on_get(self, req, resp):
         responseObj = {
@@ -10,17 +107,31 @@ class GetProjectsResource:
             "data": {}
         }
         try:
-            dbu = DBUser()
-            dbpr = DBProject()
+            projectIds = []
+            # 01. check if user is super user
             if dbu.checkIfUserIsSuperuser(req.params["kartoon-fapi-incoming"]["_id"]):
-                projectIds = [pi["_id"]["$oid"] for pi in dbpr.getAllProjectIds()]
+                # 02. 01. if yes, fetch all projectIds
+                projectIds = self.getProjectIdsForSuperuser()
             else:
-                accessibleProjects = dbu.getAccessibleProjects(req.params["kartoon-fapi-incoming"]["_id"])
-                accessibleProjectIds = [api["projectId"]["$oid"] for api in accessibleProjects]
-                publicProjects = dbpr.getPublicProjectIds()
-                publicProjectIds = [ppi["_id"]["$oid"] for ppi in publicProjects]
-                projectIds = list(set(accessibleProjectIds + publicProjectIds))
-            responseObj["data"] = dbpr.getProjectsByIds(projectIds)
+                # 02. 02. if no, fetch limited projectIds
+                projectIds = self.getProjectIdsForNonSuperuser(req.params["kartoon-fapi-incoming"]["_id"])
+            # 03. fetch projects from projectIds
+            projects = dbpr.getProjectsByIds(projectIds)
+            # 04. removeInactiveMilestones from projects
+            projects = self.removeInactiveMilestones(projects)
+            # 05. removeInactiveMembers from projects
+            projects = self.removeInactiveMembers(projects)
+            # 06. create idMap from idMapForProjectIds
+            idMap = self.idMapForProjectIds(projects)
+            # 07. update idMap with idMapForMilestoneIds
+            idMap.update(self.idMapForMilestoneIds(projects))
+            # 08. update idMap with idMapForMemberIds
+            idMap.update(self.idMapForMemberIds(projects))
+            # 09. attach projects in response
+            responseObj["data"]["projects"] = projects
+            # 10. attach idMap in response
+            responseObj["data"]["idMap"] = idMap
+            # 11. set responseId to success
             responseObj["responseId"] = 211
         except Exception as ex:
             responseObj["message"] = str(ex)
