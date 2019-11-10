3254e5cad5eada2b88e8d36f75d6ca04b0b41127
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-11 01:38:20 +0530

    added schema validation check for update pulse timeline

---
+++ utils/req_validator/validate_update_pulse_timeline.py
@@ -0,0 +1,48 @@
+validate_update_pulse_timeline_schema = {
+    "type": "object",
+    "properties": {
+        "pulseId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        },
+        "milestoneId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        },
+        "projectId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        },
+        "timeline": {
+            "type": "object",
+            "properties": {
+                "begin": {
+                    "type": "string",
+                    "pattern": "\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z"
+                },
+                "end": {
+                    "type": "string",
+                    "pattern": "\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z"
+                }
+            },
+            "required": [
+                "begin",
+                "end"
+            ],
+            "additionalProperties": False
+        }
+    },
+    "required": [
+        "pulseId",
+        "milestoneId",
+        "projectId",
+        "timeline"
+    ],
+    "additionalProperties": False
+}
