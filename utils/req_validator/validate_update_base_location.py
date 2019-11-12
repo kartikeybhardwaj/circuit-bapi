657bc08179c1b6eed0d699e10633417d59996e24
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-12 23:00:33 +0530

    added shema check to update base location

---
+++ utils/req_validator/validate_update_base_location.py
@@ -0,0 +1,15 @@
+validate_update_base_location_schema = {
+    "type": "object",
+    "properties": {
+        "locationId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        }
+    },
+    "required": [
+        "locationId"
+    ],
+    "additionalProperties": False
+}
