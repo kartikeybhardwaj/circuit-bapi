e3ec2373b98dc1ac22d8f9eca2d7df679ec16728
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-13 23:26:08 +0530

    added req validator for get-user-pulses

---
+++ utils/req_validator/validate_get_user_pulses.py
@@ -0,0 +1,14 @@
+validate_get_user_pulses_schema = {
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string",
+            "pattern": "^[0-9a-zA-Z\.\_]*$",
+            "minLength": 4,
+            "maxLength": 20
+        }
+    },
+    "required": [
+        "username"
+    ]
+}
