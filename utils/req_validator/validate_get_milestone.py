e433681330fc023983b00111838fe1fd81738da2
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-12-02 21:36:31 +0530

    added get milestone request validator

---
+++ utils/req_validator/validate_get_milestone.py
@@ -0,0 +1,20 @@
+validate_get_milestone_schema = {
+    "type": "object",
+    "properties": {
+        "projectId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        }, "milestoneId": {
+            "type": "string",
+            "pattern": "^[0-9a-z]*$",
+            "minLength": 24,
+            "maxLength": 24
+        }
+    },
+    "required": [
+        "projectId",
+        "milestoneId"
+    ]
+}
